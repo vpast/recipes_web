dc12de5878fc561c1969d743e0fdce126c4e5817
Vlad Pastushok <dalverq@gmail.com>
2022-07-22 04:50:49 +0300

    app.js reading data and overwrite html

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import * as fs from "node:fs/promises";
 import path from "node:path";
+import Handlebars from "handlebars";
 
 var dataPath = path.join("data", "chilli.json");
 
 const data = await fs.readFile(dataPath);
 const htmlData = await fs.readFile("./templates/index.html", "utf-8")
 let newData = JSON.parse(data);
-console.log(newData);
-console.log(htmlData);
+
+const template = Handlebars.compile(htmlData);
+const newHtml = template(newData);
+fs.writeFile("./output/index.html", newHtml);
+console.log(template(newData));
