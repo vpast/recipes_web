45d64e357adf955b8fb92bf08c37c90f8963fa14
Vlad Pastushok <dalverq@gmail.com>
2022-08-04 16:19:04 +0300

    new JSON files and compilation

--- app.js
+++ app.js
@@ -3,39 +3,39 @@ import path from "node:path";
 import Handlebars from "handlebars";
 import readdir from "recursive-readdir";
 
-var dataPath = path.join("data", "chilli.json");
-
-const data = await fs.readFile(dataPath);
-const htmlData = await fs.readFile(
-  path.join(".", "templates", "index.html"),
-  "utf-8"
-);
-let newData = JSON.parse(data);
 let allTemplates = {};
 
 readdir("templates").then(
   function (files) {
-    processFiles(files);
+    loadTemplates(files);
   },
   function (error) {
     console.error("EVERYTHING FUCKED UP", error);
   }
 );
 
-async function processFiles(files) {
+async function loadTemplates(files) {
   for (let i = 0; i < files.length; i++) {
-    let splitedTemplates = files[i].split("templates\\")
+    let fileName = path.basename(files[i], ".html");
     var rawHtml = await fs.readFile(files[i], "utf-8");
-    allTemplates[splitedTemplates] = Handlebars.compile(rawHtml);
-    var compiledTemplates =  allTemplates[splitedTemplates](newData);
-    console.log(compiledTemplates)
-    // console.log(filesTemplate(rawHtml))
-    // fs.writeFile(path.join(".", "output", files[i]), template(rawHtml));
+    allTemplates[fileName] = Handlebars.compile(rawHtml);
   }
-  // console.log("files are", files);
+  createPages();
 }
 
-// const template = Handlebars.compile(htmlData);
-// const newHtml = template(newData);
-// fs.writeFile(path.join(".", "output", "index.html"), newHtml);
-// console.log(template(newData));
+async function createPages() {
+  const jsonsInDir = await fs.readdir("./data");
+
+  for (let i = 0; i < jsonsInDir.length; i++) {
+    const jsonFile = await fs.readFile(
+      path.join("./data", jsonsInDir[i]),
+      "utf-8"
+    );
+    console.log(jsonsInDir[i]);
+    const newJsonData = JSON.parse(jsonFile);
+    var compiledTemplate = allTemplates["page"](newJsonData);
+    var compiledFileName = path.basename(jsonsInDir[i], ".json");
+    await fs.writeFile(path.join("output", compiledFileName + ".html"), compiledTemplate);
+    // console.log(compiledTemplate);
+  }
+}
