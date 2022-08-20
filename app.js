d776a3c7cfdab6d610abc37babf8e6c260b75533
Vlad Pastushok <dalverq@gmail.com>
2022-08-21 02:56:37 +0300

    recipe translation add cycle

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ readdir("templates").then(
 async function loadTemplates(files) {
   for (let i = 0; i < files.length; i++) {
     let fileName = path.basename(files[i], ".html");
-    var rawHtml = await fs.readFile(files[i], "utf-8");
+    let rawHtml = await fs.readFile(files[i], "utf-8");
     allTemplates[fileName] = Handlebars.compile(rawHtml);
   }
   createPages();
@@ -25,17 +25,36 @@ async function loadTemplates(files) {
 
 async function createPages() {
   const jsonsInDir = await fs.readdir("./data/recipes");
+  const ingredientsData = JSON.parse(
+    await fs.readFile("./data/ingredients.json")
+  );
+  const translations = JSON.parse(await fs.readFile("./data/texts.json"));
 
   for (let i = 0; i < jsonsInDir.length; i++) {
     const jsonFile = await fs.readFile(
       path.join("./data/recipes", jsonsInDir[i]),
       "utf-8"
     );
-    console.log(jsonsInDir[i]);
-    const newJsonData = JSON.parse(jsonFile);
-    var compiledTemplate = allTemplates["page"](newJsonData);
-    var compiledFileName = path.basename(jsonsInDir[i], ".json");
-    await fs.writeFile(path.join("output", compiledFileName + ".html"), compiledTemplate);
-    console.log(compiledTemplate);
+    // console.log(jsonsInDir[i]);
+    const recipe = JSON.parse(jsonFile);
+    const ingredientList = recipe["ingredients"];
+    
+    for (let j = 0; j < ingredientList.length; j++) {
+      const item = ingredientList[j];
+      const name = item["name"];
+      item["english"] = ingredientsData[name]["translations"]["en_US"];
+      console.log(item);
+    }
+    
+    let compiledTemplate = allTemplates["page"](recipe);
+
+    // console.log(recipe["ingredients"][0]["gram"]);
+    let compiledFileName = path.basename(jsonsInDir[i], ".json");
+
+    await fs.writeFile(
+      path.join("output", compiledFileName + ".html"),
+      compiledTemplate
+    );
+    // console.log(compiledTemplate);
   }
 }
